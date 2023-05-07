c1d47268ae37f9191effc7c8eabc3ab773bb93a8
jaxonhill <jaxoncharleshill@gmail.com>
2023-05-06 21:39:02 -0700

    Add sliders for velocity and acceleration and theta for vector components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,59 @@
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import { NextFont } from "@next/font";
+import { NextFont } from "next/dist/compiled/@next/font";
+import { useState } from "react";
+import Image from "next/image";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter: NextFont = Inter({ subsets: ["latin"] });
 
+function toRadians(angle: number) {
+	return angle * (Math.PI / 180);
+}
+
 export default function Home(): JSX.Element {
+	const [displacement, setDisplacement] = useState(-10);
+	const [time, setTime] = useState(10);
+	const [theta, setTheta] = useState(45);
+	const [velocity, setVelocity] = useState(10);
+	const [time2, setTime2] = useState(10);
+
+	// Do the velocity calculation and rounded it to 2 decimal places always
+	let velocityAnswer = (
+		Math.round((displacement / time) * 100) / 100
+	).toFixed(2);
+
+	let accelerationAnswer = (
+		Math.round((velocity / time2) * 100) / 100
+	).toFixed(2);
+
+	// Do the calculation for vx and vy
+	let vx = Math.round(50 * Math.cos(toRadians(theta)) * 100) / 100;
+	let vy = Math.round(50 * Math.sin(toRadians(theta)) * 100) / 100;
+
 	return (
 		<>
 			<article
 				className={`mx-auto w-full max-w-3xl my-16 ${inter.className}`}
 			>
-				<div className="bg-slate-800 p-8 mb-12 flex flex-col gap-2 rounded-2xl">
+				<div className="bg-slate-800 p-8 mb-4 flex flex-col gap-2 rounded-2xl">
 					<h2 className="text-5xl text-white font-bold">
 						2D Kinematics
 					</h2>
-					<p className="text-slate-200">By Jaxon Hill</p>
+					<p className="text-slate-200 text-xl">By Jaxon Hill</p>
+				</div>
+				<div className="mb-12">
+					<p className="text-slate-800">
+						<em>
+							An overview of 2D Kinematics at large. This includes
+							introducing vectors, displacement, velocity,
+							acceleration to then explain how to apply these
+							vectors in two dimensional space. I wrote this paper
+							almost as if it were an introduction / guide, as it
+							is something I wish I could have utilized to help
+							grasp these topics when I first learned them.
+						</em>
+					</p>
 				</div>
 				<main className="prose lg:prose-lg prose-h2:mb-6 prose-h3:mb-4">
 					<h2>Introduction</h2>
@@ -84,23 +122,75 @@ export default function Home(): JSX.Element {
 					<p>
 						How long did it take us to get there? This would
 						describe our velocity. Say we walked 10 meters to the
-						west in 10 seconds (with no twists or turns, it was just
-						displacement). If we divide 10 meters by 10 seconds, we
-						are traveling at -1 m/s. This perplexed me at first, but
-						remember what we stated about vectors. In the context of
-						the problem, it is crucial to understand that we have
-						gone 10 meters to the left (the negative direction). Try
-						moving the sliders for both the displacement and the
-						time to see how they interact.{" "}
+						west in 10 seconds (with no twists or turns). If we
+						divide 10 meters by 10 seconds, we are traveling at -1
+						m/s. How can velocity be negative? This perplexed me at
+						first, but remember what we know is true about vectors.
+						In the context of the problem, it is crucial to
+						understand that we have traveled 10 meters to the left
+						(the negative direction). Try moving the sliders for
+						both the displacement and the time to see how they
+						interact.{" "}
 						<strong className="font-bold">
 							Note that if you travel further in a shorter amount
-							of time, the magnitude of your velocity is getting
-							bigger and bigger.
+							of time, the magnitude of your velocity is
+							increasing.
 						</strong>{" "}
 						This makes logical sense as we physically have to be
 						moving faster in order to cover the same distance in
 						less time.
 					</p>
+					<div className="w-full grid grid-cols-8 gap-8">
+						<div className="col-span-1"></div>
+						<div className="col-span-2">
+							<div className="py-4 flex flex-col gap-2">
+								<strong className="font-bold">
+									Displacement
+								</strong>
+								<input
+									id="displacement"
+									min="-50"
+									max="50"
+									step="1"
+									type="range"
+									value={displacement}
+									onChange={(e) =>
+										setDisplacement(Number(e.target.value))
+									}
+									className="h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+								></input>
+							</div>
+							<div className="py-4 flex flex-col gap-2">
+								<strong className="font-bold">Time</strong>
+								<input
+									id="time"
+									type="range"
+									min="1"
+									max="25"
+									step="1"
+									value={time}
+									onChange={(e) =>
+										setTime(Number(e.target.value))
+									}
+									className="h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+								></input>
+							</div>
+						</div>
+						<div className="flex col-span-5 justify-center gap-8">
+							<div className="flex flex-col h-full justify-center text-2xl">
+								<div className="py-4 w-full flex justify-center border-b-2 border-slate-500">
+									{displacement} m
+								</div>
+								<div className="py-4 w-full flex justify-center">
+									{time} s
+								</div>
+							</div>
+							<div className="h-full flex items-center gap-6">
+								<p className="text-3xl">=</p>
+								<p className="text-5xl">{velocityAnswer} m/s</p>
+							</div>
+						</div>
+					</div>
 					<h3>Acceleration</h3>
 					<p>
 						However, this was a constant speed. Now imagine if our
@@ -116,6 +206,59 @@ export default function Home(): JSX.Element {
 							amount of time, your acceleration will increase!
 						</strong>{" "}
 					</p>
+					<div className="w-full grid grid-cols-8 gap-8">
+						<div className="col-span-1"></div>
+						<div className="col-span-2">
+							<div className="py-4 flex flex-col gap-2">
+								<strong className="font-bold">
+									Velocity (&#916;v)
+								</strong>
+								<input
+									id="velocity"
+									min="-50"
+									max="50"
+									step="1"
+									type="range"
+									value={velocity}
+									onChange={(e) =>
+										setVelocity(Number(e.target.value))
+									}
+									className="h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+								></input>
+							</div>
+							<div className="py-4 flex flex-col gap-2">
+								<strong className="font-bold">Time</strong>
+								<input
+									id="time2"
+									type="range"
+									min="1"
+									max="25"
+									step="1"
+									value={time2}
+									onChange={(e) =>
+										setTime2(Number(e.target.value))
+									}
+									className="h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+								></input>
+							</div>
+						</div>
+						<div className="flex col-span-5 justify-center gap-8">
+							<div className="flex flex-col h-full justify-center text-2xl">
+								<div className="py-4 w-full flex justify-center border-b-2 border-slate-500">
+									{velocity} m/s
+								</div>
+								<div className="py-4 w-full flex justify-center">
+									{time2} s
+								</div>
+							</div>
+							<div className="h-full flex items-center gap-6">
+								<p className="text-3xl">=</p>
+								<p className="text-5xl">
+									{accelerationAnswer} m/s<sup>2</sup>
+								</p>
+							</div>
+						</div>
+					</div>
 					<h2>1D vs. 2D</h2>
 					<p>
 						Now, you’re probably thinking, that’s great — but
@@ -123,12 +266,64 @@ export default function Home(): JSX.Element {
 						discussed so far: vectors have an x and y component, and
 						displacement, velocity, and acceleration are all
 						vectors. Therefore, we can solve problems relating to
-						each one of these portions of kinematics if we simply
+						each one of these portions of kinematics if we simply{" "}
 						<strong className="font-bold">
 							break the problem down into the two dimensions and
 							break every vector down into its components.
 						</strong>
 					</p>
+					<p>
+						Say we had some velocity (v) and some angle (&theta;)
+						counterclockwise above the x-axis. How would we break
+						this into its components? Again, it is important to
+						remember our trigonometric rules. We have a right
+						triangle, thus we can use the our sin, cos, and tan
+						properties. Given that v is our hypotenuse, we can then
+						apply these trigonometric ratios to derive the x and y
+						components of v.
+					</p>
+					<div className="grid grid-cols-6 font-bold text-2xl gap-8 justify-items-center items-center">
+						<div className="col-span-1"></div>
+						<div className="col-span-2">
+							v<sub>x</sub> = v * cos(&theta;){" "}
+						</div>
+						<div className="col-span-2">
+							v<sub>y</sub> = v * sin(&theta;)
+						</div>
+						<div className="col-span-1"></div>
+					</div>
+					<p>
+						Edit the angle (&theta;) for a set velocity of 50 m/s.
+						Notice how these components change. Notice how when
+						&theta; is 0&deg; our v<sub>y</sub> becomes 0. This is
+						because we are moving horizontally! What do you think
+						happens when &theta; is 90&deg;?
+					</p>
+					<div className="w-full flex flex-col items-center gap-2 pb-8">
+						<strong className="font-bold">
+							(&theta;) = {theta}&deg;
+						</strong>
+						<input
+							id="displacement"
+							min="0"
+							max="90"
+							step="1"
+							type="range"
+							value={theta}
+							onChange={(e) => setTheta(Number(e.target.value))}
+							className="h-2 w-1/4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+						></input>
+					</div>
+					<div className="grid grid-cols-6 font-bold text-2xl gap-8 justify-items-center items-center">
+						<div className="col-span-1"></div>
+						<div className="col-span-2">
+							v<sub>x</sub> = {vx} m/s
+						</div>
+						<div className="col-span-2">
+							v<sub>y</sub> = {vy} m/s
+						</div>
+						<div className="col-span-1"></div>
+					</div>
 					<h2>Translational vs. Rotational Motion</h2>
 					<p>
 						In two dimensional kinematics, there are two different
@@ -141,9 +336,26 @@ export default function Home(): JSX.Element {
 						object where all parts are moving in the same direction.
 						The object is not rotating. This could be compared to a
 						person walking forward, a ball being thrown through the
-						air, or a cannonball being shot off a wall.
+						air, or a cannonball being shot off a wall.{" "}
+						<strong className="font-bold">
+							Here are our given equations for translational
+							motion:
+						</strong>
 					</p>
-					<img src="" alt="" />
+					<div className="flex flex-col gap-4 items-center font-bold text-2xl">
+						<div>
+							x<sub>f</sub> = x<sub>0</sub> + v<sub>0</sub>t +
+							(1/2)at<sup>2</sup>
+						</div>
+						<div>
+							v<sub>f</sub> = v<sub>0</sub> + at
+						</div>
+						<div>
+							v<sub>f</sub>
+							<sup>2</sup> = v<sub>0</sub>
+							<sup>2</sup> + 2a(x<sub>f</sub> - x<sub>0</sub>)
+						</div>
+					</div>
 					<h3>Rotational Motion</h3>
 					<p>
 						Rotational motion is motion where the object is spinning
@@ -163,8 +375,26 @@ export default function Home(): JSX.Element {
 						into that circular path. This can be confusing because
 						even if we are moving at a constant speed, we have
 						acceleration as the direction and thus components of the
-						centripetal acceleration vector are always changing.
+						centripetal acceleration vector are always changing.{" "}
+						<strong>
+							Here are our given equations for rotational motion.
+						</strong>
 					</p>
+					<div className="flex flex-col gap-4 items-center font-bold text-2xl">
+						<div>
+							&theta;<sub>f</sub> = &theta;<sub>0</sub> + &omega;
+							<sub>0</sub>t + (1/2)&alpha;t<sup>2</sup>
+						</div>
+						<div>
+							&omega;<sub>f</sub> = &omega;<sub>0</sub> + &alpha;t
+						</div>
+						<div>
+							&omega;<sub>f</sub>
+							<sup>2</sup> = &omega;<sub>0</sub>
+							<sup>2</sup> + 2&alpha;(&theta;<sub>f</sub> -
+							&theta;<sub>0</sub>)
+						</div>
+					</div>
 					<h2>Projectile Motion</h2>
 					<p>
 						Projectile motion is frequently discussed when talking
